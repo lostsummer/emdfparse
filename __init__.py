0a9c6b862f1a94a4d4550a378cebd261ed7e60d9
王昱鑫 <wangyuxin0623@emoney.cn>
2018-02-05 16:30:13 +0800

    docstring add

--- __init__.py
+++ __init__.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
-"""docstring
+"""
+dfparse tool
+~~~~~~~~~~~~
+
+dfparse is a moudle for parse DS data file day.dat, min1.dat, minute.dat, and so on.
+usage:
+
+    >>> from dfparse import DataFile, Day
+    >>> df = DataFile('/usr/local/EMoney/Data/Day.dat', Day)
+    >>> for id, tms in df.items():
+            print('id: {}'.format(id))
+            print(tms)
+
+    id: 1
+    time:20171009    open:3403245     high:3410170     low :3366965     close:3374378     volume:191736057   amount:227440594944
+    time:20171016    open:3393205     high:3400511     low :3374768     close:3378470     volume:174330620   amount:221650690048
+    ...
 """
 
 
